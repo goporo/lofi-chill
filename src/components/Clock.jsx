2231abb0aafab2e71a716c1839b6122666fe31f5
goporo <goporolo1@gmail.com>
2022-09-01 23:35:49 +0700

    responsive

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const Clock = () => {
     return (
-        <div className='flex mb-10'>
+        <div className='flex mb-5 md:mb-10'>
             <div className='flex flex-row mr-20'>
-                <div className='text-8xl mx-3 p-10 text-white bg-black 
+                <div className='text-6xl md:text-8xl p-6 md:p-10 mx-3 text-white bg-black 
             bg-opacity-75 rounded-xl cursor-pointer hover:opacity-70'>
                     2
                 </div>
-                <div className='text-8xl mx-3 p-10 text-white bg-black 
+                <div className='text-6xl md:text-8xl p-6 md:p-10 mx-3 text-white bg-black 
             bg-opacity-75 rounded-xl cursor-pointer hover:opacity-70'>
                     1
                 </div>
             </div>
             {/*  */}
             <div className='flex flex-row'>
-                <div className='text-8xl mx-3 p-10 text-white bg-black 
+                <div className='text-6xl md:text-8xl p-6 md:p-10 mx-3 text-white bg-black 
             bg-opacity-75 rounded-xl cursor-pointer hover:opacity-70'>
                     3
                 </div>
-                <div className='text-8xl mx-3 p-10 text-white bg-black 
+                <div className='text-6xl md:text-8xl p-6 md:p-10 mx-3 text-white bg-black 
             bg-opacity-75 rounded-xl cursor-pointer hover:opacity-70'>
                     0
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { HiMenu } from "react-icons/hi";
 const Header = () => {
     return (
         <header>
-            <div className="flex flex-row items-center justify-between mx-20">
-                <img className='cursor-pointer' width={180} height={100} src={images.logo} alt="logo" />
+            <div className="flex flex-row items-center justify-between mx-0 md:mx-10">
+                <img className='cursor-pointer' width={110} src={images.logo} alt="logo" />
                 <div className='flex flex-row '>
                     <div>
                         <div className='text-3xl mx-3 p-3 text-white bg-black 

1934a5f9c8a183531b8ba83f3bc21ad17c94d48c
goporo <goporolo1@gmail.com>
2022-09-03 12:45:52 +0700

    .

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,17 +1,17 @@
 const images = {
     lofis: [
-        // { img: require('./lofi0.png') },
-        // { img: require('./lofi1.gif') },
-        // { img: require('./lofi2.webp') },
+        { img: require('./lofi0.png') },
+        { img: require('./lofi1.gif') },
+        { img: require('./lofi2.webp') },
         // { img: require('./lofi3.gif') },
         // { img: require('./lofi4.jpg') },
         // { img: require('./lofi5.png') },
         // { img: require('./lofi6.jpg') },
         // { img: require('./lofi7.jpg') },
     ],
-    // logo: require('./logo.gif'),
-    // pomodoro: require('./pomodoro_technique.jpg'),
-    // tomato: require('./tomato.jpg'),
+    logo: require('./logo.gif'),
+    pomodoro: require('./pomodoro_technique.jpg'),
+    tomato: require('./tomato.jpg'),
 };
 
 export default images;
